ef0abedc776d7aa47432a927fafbf26f54a81679
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-14 22:55:59 -0400

    Too many timezone config options in Python ugh

--- time_display/views.py
+++ time_display/views.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
-from time import strftime
+from django.utils import timezone
+import datetime
 
 def index(request):
     
     context = {
         "title": "The current time and date:",
-        "date": strftime("%A %B %d"),
-        "time": strftime("%-I:%M %p %Z")
+        "datetime": datetime.datetime.now()
         }
 
     return render(request, "time_display/time.html", context)
