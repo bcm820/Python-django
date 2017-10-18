137978f8055b8405986f1e9529d706ea174a1dab
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-17 22:33:31 -0400

    Posted a semi-RESTful CRUD demo

--- users/models.py
+++ users/models.py
@@ -7,6 +7,3 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
--- users/views.py
+++ users/views.py
@@ -1,54 +1,42 @@
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
 
-# practice form validation
-# create Django form version with its form validation?
 
-def index():
+def index(request):
     users = { "users": User.objects.all() }
     return render(request, 'users/index.html', users)
 
-def new():
+def new(request):
     users = { "users": User.objects.all() }
     return render(request, 'users/new.html', users)
 
-def show(id):
+def show(request, id):
     user = { "user": User.objects.get(id=id) }
     return render(request, 'users/show.html', user)
 
-def edit(id):
+def edit(request, id):
     user = { "user": User.objects.get(id=id) }
     return render(request, 'users/edit.html', user)
 
-def create():
+def create(request):
 
     User.objects.create(
         name = request.POST['name'],
-        email = request.POST['email']
-    )
-    
+        email = request.POST['email'])
+
     id = User.objects.last().id
-    return redirect('/users/' + id)
-    
 
-# UPDATE: Update record by ID
-@app.route('/users/<user_id>/update/', methods=['POST'])
-def update(user_id):
-    
-    query = "UPDATE users SET name = :name, email = :email WHERE id = :id"
-    data = {
-        'name': request.form['name'],
-        'email': request.form['email'],
-        'id': user_id
-        }
-    mysql.query_db(query, data)
-    return redirect('/users/{}/'.format(user_id))
+    return redirect('/users/' + str(id))
 
+def update(request, id):
 
-# DESTROY: Delete record by ID
-@app.route('/users/<user_id>/destroy/', methods=['POST'])
-def destroy(user_id):
-    query = "DELETE FROM users WHERE id = :id"
-    data = {'id': user_id}
-    mysql.query_db(query, data)
+    user = User.objects.get(id=id)
+    user.name = request.POST['name']
+    user.email = request.POST['email']
+
+    return redirect('/users/' + str(id))
+
+def destroy(request, id):
+    User.objects.get(id=id).delete()
     return redirect('/users/')
