b511838b0790fc6a402e1b20203840105c24f171
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-10-18 07:42:33 -0400

    New Django project

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^new/$', views.new),
+    url(r'^create/$', views.create),
     url(r'^(?P<id>\d+)/$', views.show),
     url(r'^(?P<id>\d+)/edit/$', views.edit),
-    url(r'^create/$', views.create),
     url(r'^(?P<id>\d+)/update/$', views.update),
     url(r'^(?P<id>\d+)/destroy/$', views.destroy)
 ]
